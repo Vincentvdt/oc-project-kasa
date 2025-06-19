b283a89ec15701b729d8f924516dff280cfbcbc1
vincentvdt <vincent.vidot3@gmail.com>
2025-06-20 01:31:21 +0200

    test: remove chunkArray test file as it is no longer needed

--- src/utils/chunkArray.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { chunkArray } from './chunkArray'
-
-describe('chunkArray', () => {
-  it('splits array into chunks of given size', () => {
-    expect(chunkArray([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]])
-  })
-})
