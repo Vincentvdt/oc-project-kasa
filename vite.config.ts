3b421f112ca4393b4230f6943a94941e76875377
vincentvdt <vincent.vidot3@gmail.com>
2025-06-19 17:31:21 +0200

    update Vite config and package scripts: set `base` for GitHub Pages deployment and fix `deploy` output directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'oc-project-kasa',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   server: {
     port: 5173,
-    open: true
+    open: true,
   },
   build: {
     sourcemap: true,
-    outDir: 'dist'
-  }
-});
+    outDir: 'dist',
+  },
+})
